4d4aa738c43ac06f213ca9f388091c26b0242173
ViStamelou <vasilikist.97@gmail.com>
2024-11-22 16:35:49 +0200

    fix 4

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mikro-website/', // Set the base path for GitHub Pages
   css: {
     modules: {
       localsConvention: 'camelCase',
